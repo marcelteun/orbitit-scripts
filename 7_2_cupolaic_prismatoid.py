9ea5cf495701e4d13060d6b5ee8c32e4ed07f813
Marcel Tünnissen <marcelteun@gmail.com>
2025-06-27 11:38:19 +0200

    Clean up script to use arg parser

--- 7_2_cupolaic_prismatoid.py
+++ 7_2_cupolaic_prismatoid.py
@@ -1,31 +1,42 @@
 # Script to generate a cupalaic prismatoid based on a heptagram
+import argparse
+from pathlib import Path
+import sys
+
 from math import cos, pi, sin, sqrt
 from orbitit import geom_3d as geom
 from orbitit.colors import STD_COLORS as cols
 from orbitit import geomtypes
 
-# TODO: use optional parameters for these.
+parser = argparse.ArgumentParser(
+    description="Generate an off file for {7/2} based pseudo-cupolaic prismatoids",
+)
+parser.add_argument("filename", help="Path to the off file to be save.")
+parser.add_argument(
+    "-k", "--allow_holes",
+    action="store_true",
+    help="If specified the {n/m} polygon will saved using n vertices, which results in holes in "
+    "orbitit for the parts that have even coverage due to the stencil buffer.",
+)
+parser.add_argument(
+    "-s", "--crossed_squares",
+    action="store_true",
+    help="If specified the {n/m} polygon will be kept as is, which results in holes in orbitit for "
+)
+args = parser.parse_args()
 
-# Set to True and the concave points of the heptagram will be added to the vertices and polygon will
-# be the outline with 14 vertices. This prevents the stencil buffer to remove the central heptagon
-# when drawing the heptagram.
-REPLACE_HEPTAGRAM = True
-
-# If set to True then the triangles will be equilateral. Otherwise the crossed square, the bow-ties,
-# will fit into a square.
-USE_EQUILATERAL_TRIANGLES = True
+# TODO: generalise this script for any n/m
 
 # Vertices
 # This assumes the side of the heptagon has length 2
-radius = 1 / sin(pi / 7)
-two_pi = 2 * pi
-rho = 2 * radius * sin(two_pi / 7)
+RADIUS = 1 / sin(pi / 7)
+TWO_PI = 2 * pi
+DIAGONAL = 2 * RADIUS * sin(TWO_PI / 7)
 
-if USE_EQUILATERAL_TRIANGLES:
-    z = sqrt(rho**2 - 4) / 2  # 4 = squared edge length
+if not args.crossed_squares:
+    HEIGHT = sqrt(DIAGONAL**2 - 4) / 2  # 4 = squared edge length
 else:
-    z = 1  # half edge length
-print(f"rho = {rho}")
+    HEIGHT = 1  # half edge length
 #  Y ^
 #    |           v0
 #    |    v6            v1
@@ -35,7 +46,9 @@ print(f"rho = {rho}")
 #    |
 #    |
 #    |       v4     v3
-vs = [geom.vec(radius * cos(i * two_pi / 7), radius * sin(i * two_pi / 7), z) for i in range(7)]
+vs = [
+    geom.vec(RADIUS * cos(i * TWO_PI / 7), RADIUS * sin(i * TWO_PI / 7), HEIGHT) for i in range(7)
+]
 vs.extend([geom.vec(v[0], v[1], -v[2]) for v in vs])
 # Sides and colours
 faces = [
@@ -56,7 +69,7 @@ faces = [
     [6, 13, 0, 7],
 ]
 
-if REPLACE_HEPTAGRAM:
+if not args.keep_n_gram:
     heptagram = geom.Face([vs[faces[0][i]] for i in range(7)])
     with geomtypes.FloatHandler(8):
         extended_heptagram = heptagram.outline
@@ -68,5 +81,13 @@ shape = geom.SimpleShape(
     vs,
     faces,
     colors=(cols, [0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2]),
+    name="7/2 pseudo-cupolaic prismatoid",
 )
-print(shape.to_off())
+filepath = Path(args.filename)
+if filepath.is_file():
+    yes_or_no = input("File exists. Overwrite? y/N\n")
+    if yes_or_no == "" or yes_or_no.lower()[0] != "y":
+        sys.exit(1)
+
+with open(args.filename, "w") as fd:
+    fd.write(shape.to_off())
